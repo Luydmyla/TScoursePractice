61f3f113a55f15498b1bb98ffe63e0d987293d91
Luydmyla <lytic.ok@gmail.com>
2022-09-30 00:52:11 +0300

    lesson4

---
+++ dist/button.js
@@ -0,0 +1,6 @@
+"use strict";
+const button = document.querySelector('#buttonId');
+button.addEventListener('click', () => {
+    console.log("click");
+});
+//# sourceMappingURL=button.js.map
---
+++ src/button.ts
@@ -0,0 +1,7 @@
+
+// const some2 = "str2";
+// console.log(some2)
+const button = document.querySelector('#buttonId')!;
+button.addEventListener('click', () => {
+    console.log("click")
+})
